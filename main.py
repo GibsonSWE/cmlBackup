f5d8ec52f86ff40eab7cad4be47c876dd678d79b
Jacob Henrisen <jacob.henriksen94@gmail.com>
2024-04-11 22:53:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from src import constants as const
+from src import cml_api as cml
+
+
+
+
+def main():
+    labs = cml.get_labs()
+
+    for lab in labs:
+        pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/cml_api.py
@@ -0,0 +1,16 @@
+
+
+#Endpoints to implement:
+
+#POST /authenticate
+
+#GET /labs
+#GET /labs/{lab_id}
+#GET /labs/{lab_id}/state
+#GET /labs/{lab_id}/check_if_converged
+#GET /labs/{lab_id}/download
+
+#PUT /labs/{lab_id}/start
+#PUT /labs/{lab_id}/stop
+#PUT /labs/{lab_id}/extract_configuration
+
