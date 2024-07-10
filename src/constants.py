0dc2043f560c61df812862b6f01c017ed9896da8
Jacob Henriksen <jacob.henriksen94@gmail.com>
2024-07-10 12:22:48 +0200

    Changed backup path

--- src/constants.py
+++ src/constants.py
@@ -2,4 +2,4 @@
 release_data = {'version': 'Beta', 'date': '2024-06-23'}
 
 CML_URL = 'https://10.0.0.8/api/v0'
-BACKUP_PATH = '/home/gibson/cml_backups/'
+BACKUP_PATH = '/home/gibson/cml/cml_backups/'
